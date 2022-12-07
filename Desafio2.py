d8d651735eb36c0fb21bfcfd86a523e936f8db40
Camila Evangelista <94421173+camilaeva1@users.noreply.github.com>
2022-12-07 05:18:35 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,26 +1,26 @@
-class point(object):
-	def __init__(self, x, y):
-		self.x = x
+class Topo(object): #Classe topo, representando os topos das árvores no plano cartesiano
+	def __init__(self, x, y): #Método que inicializar as instâncias das coordenadas x, y
+		self.x = x 
 		self.y = y
-	def __lt__(self, other):
+	def __lt__(self, other): #Método que compara a posição dos topos no plano cartesiano
 		return self.x < other.x or (self.x == other.x and self.y < other.y)
 
-def cross (p0, p1, p2):
+def prodvetor (p0, p1, p2): 
 	return (p1.x - p0.x)*(p2.y - p0.y) - (p2.x - p0.x)*(p1.y - p0.y)
 
-n = int(input())
-p = [point(0,0) for i in range(n)]
-ch = [0 for i in range(n)]
+n = int(input()) #Número de árvores
+t = list(Topo(0,0) for i in range(n)) #lista de topos das árvores
+ch = list(0 for i in range(n))
 
 for i in range(n):
-	l = [int(x) for x in input().split()]
-	p[i].x, p[i].y = l[0], l[1]
+	l = list(int(x) for x in input().split())
+	t[i].x, t[i].y = l[0], l[1] #Atribui (x,y) a cada topo
 
-p.sort()
-nch = 0
+t.sort() #Ordenar a lista de topos
+nch = 0 
 
 for i in range(n):
-	while (nch > 1 and cross(p[ch[nch-2]], p[ch[nch-1]], p[i]) >= 0):
+	while (nch > 1 and prodvetor(t[ch[nch-2]], t[ch[nch-1]], t[i]) >= 0): #Realiza a verificação, enquantro houver mais de um ponto na lista e analisando os dois últimos topos em relação ao que o macaco está (t[i])
 		nch -= 1;
 	ch[nch] = i
 	nch += 1;
